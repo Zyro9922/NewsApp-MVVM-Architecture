df63703380c70affed77a6513990b979d572ec08
Zyro9922 <alihasan9922@gmail.com>
2023-12-04 16:28:55 +0530

    NewsSource update to use UiState object

--- app/src/main/java/com/alihasan/newsapp_mvvm_architecture/ui/newssource/NewsSourceActivity.kt
+++ app/src/main/java/com/alihasan/newsapp_mvvm_architecture/ui/newssource/NewsSourceActivity.kt
@@ -2,12 +2,19 @@ package com.alihasan.newsapp_mvvm_architecture.ui.newssource
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.alihasan.newsapp_mvvm_architecture.NewsApplication
 import com.alihasan.newsapp_mvvm_architecture.databinding.ActivityNewsSourceBinding
 import com.alihasan.newsapp_mvvm_architecture.di.component.DaggerNewsSourceActivityComponent
 import com.alihasan.newsapp_mvvm_architecture.di.module.NewsSourceActivityModule
+import com.alihasan.newsapp_mvvm_architecture.ui.base.UiState
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class NewsSourceActivity : AppCompatActivity() {
@@ -26,7 +33,7 @@ class NewsSourceActivity : AppCompatActivity() {
         setContentView(binding.root)
         injectDependencies()
         initializeRecyclerView()
-        observeViewModelAndFetchData()
+        setupObserver()
     }
 
     private fun initializeRecyclerView(){
@@ -44,10 +51,25 @@ class NewsSourceActivity : AppCompatActivity() {
             .newsSourceActivityModule(NewsSourceActivityModule(this)).build().inject(this)
     }
 
-    private fun observeViewModelAndFetchData() {
-        newsSourceViewModel.getNewsSourcesList().observe(this) { newsSources ->
-            newsSources?.let {
-                newsSourceAdapter.updateData(it)
+    private fun setupObserver() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                newsSourceViewModel.uiState.collect {
+                    when (it) {
+                        is UiState.Success -> {
+                            newsSourceAdapter.updateData(it.data)
+                            binding.recyclerView.visibility = View.VISIBLE
+                        }
+                        is UiState.Loading -> {
+                            binding.recyclerView.visibility = View.GONE
+                        }
+                        is UiState.Error -> {
+                            // Handle Error
+                            Toast.makeText(this@NewsSourceActivity, it.message, Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+                }
             }
         }
 
--- app/src/main/java/com/alihasan/newsapp_mvvm_architecture/ui/newssource/NewsSourceViewModel.kt
+++ app/src/main/java/com/alihasan/newsapp_mvvm_architecture/ui/newssource/NewsSourceViewModel.kt
@@ -7,18 +7,19 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.alihasan.newsapp_mvvm_architecture.data.model.SourceModel.Source
 import com.alihasan.newsapp_mvvm_architecture.data.repository.SourceRepository
+import com.alihasan.newsapp_mvvm_architecture.ui.base.UiState
 import com.alihasan.newsapp_mvvm_architecture.utils.AppConstant
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class NewsSourceViewModel @Inject constructor(
-    private val newsRepository: SourceRepository // Replace with your actual repository
+    private val newsRepository: SourceRepository
 ) : ViewModel() {
 
-    private val _newsSources = MutableLiveData<List<Source>>()
-    fun getNewsSourcesList(): MutableLiveData<List<Source>> {
-        return _newsSources
-    }
+    private val _uiState = MutableStateFlow<UiState<List<Source>>>(UiState.Loading)
+    val uiState: StateFlow<UiState<List<Source>>> = _uiState
 
     private val _refreshingState = MutableLiveData<Boolean>()
     val refreshingState: LiveData<Boolean> get() = _refreshingState
@@ -32,9 +33,10 @@ class NewsSourceViewModel @Inject constructor(
         viewModelScope.launch {
             try {
                 val sourcesResponse = newsRepository.getSources(AppConstant.COUNTRY, AppConstant.LANGUAGE)
-                _newsSources.value = sourcesResponse.sources
+                _uiState.value = UiState.Success(sourcesResponse.sources)
             } catch (e: Exception) {
                 // Handle the error
+                _uiState.value = UiState.Error(e.toString())
             } finally {
                 _refreshingState.value = false
             }
